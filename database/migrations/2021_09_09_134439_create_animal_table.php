588bbbaac8a32df489eb10ae45cdf03faafc8028
Aditya Prasetyo <adty404@gmail.com>
2021-09-09 20:49:01 +0700

    Create Migration Animal table

---
+++ database/migrations/2021_09_09_134439_create_animal_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animal', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('farm_id');
+            $table->string('name');
+            $table->string('weight');
+            $table->string('height');
+            $table->string('condition');
+            $table->string('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animal');
+    }
+}
