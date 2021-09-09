57518fca4337601c2ce7260c3f002953f71f511d
Aditya Prasetyo <adty404@gmail.com>
2021-09-09 20:27:02 +0700

    Create Migration Office table

---
+++ database/migrations/2021_09_09_121651_create_office_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('office', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('code');
+            $table->string('name');
+            $table->string('address');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('pic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('office');
+    }
+}
