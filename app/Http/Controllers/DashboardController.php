e72b55c7e311aac9543ad98a5a013da821cd6c3f
Aditya Prasetyo <adty404@gmail.com>
2021-09-23 18:58:34 +0700

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
+use App\Models\Category;
+use App\Models\Farm;
+use App\Models\Office;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -12,14 +17,36 @@ class DashboardController extends Controller
     }
 
     public function index(){
-        // $user = auth()->user();
+        $user = auth()->user();
 
-        // if($user->role == 'master'){
-        //     return view('pages.admin.dashboard');
-        // }
+        if($user['role'] == 'master'){
+            $one = User::all()->count();
+            $two = Office::all()->count();
+            $three = Farm::all()->count();
+            $four = Animal::all()->count();
+        }else if($user['role'] == 'super-admin'){
+            $office_code = auth()->user()->office->code;
+            $office_id = auth()->user()->office->id;
 
-        return view('pages.admin.dashboard');
+            $one = User::has('office')->where('code', $office_code)->count();
+            $two = Farm::where('office_id', $office_id)->count();
+            $three = Category::all()->count();
+            $four = auth()->user()->office->animal->count();
+        }else if($user['role'] == 'admin' || $user['role'] == 'operator'){
+            $farm_id = auth()->user()->farm->id;
+            $office_id = auth()->user()->farm->office_id;
 
-        return redirect('/');
+            $one = Office::where('id', $office_id)->first();
+            $two = Farm::where('id', $farm_id)->first();
+            $three = Category::all()->count();
+            $four = Animal::where('farm_id', $farm_id)->count();
+        }
+
+        return view('pages.admin.dashboard', [
+            'one' => $one,
+            'two' => $two,
+            'three' => $three,
+            'four' => $four,
+        ]);
     }
 }
---
+++ resources/views/pages/admin/dashboard/super-admin.blade.php
@@ -0,0 +1,103 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        {{-- @if (auth()->user()->role == 'super-admin')
+                <li class="breadcrumb-item active">Dashboard v1</li>
+              @endif --}}
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $one }}</h3>
+
+                            <p>Users Office</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="{{ route('user-all.index') }}" class="small-box-footer"><i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $two }}</h3>
+
+                            <p>Farms</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="{{ route('office.index') }}" class="small-box-footer"><i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $three }}</h3>
+
+                            <p>Categories</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="#" class="small-box-footer"><i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ $four }}</h3>
+
+                            <p>Animals</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="{{ route('animal.index') }}" class="small-box-footer"><i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+            <!-- Main row -->
+            <div class="row">
+                <!-- Left col -->
+
+                <!-- /.Left col -->
+                <!-- right col (We are only adding the ID to make the widgets sortable)-->
+
+                <!-- right col -->
+            </div>
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
