b77c5e151580a9e84644e103f429b38e278c0450
Aditya Prasetyo <adty404@gmail.com>
2021-09-23 19:20:29 +0700

    OperatorSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(AnimalSeeder::class);
+        $this->call(OperatorSeeder::class);
     }
 }
